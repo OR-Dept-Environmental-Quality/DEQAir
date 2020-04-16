db9fc5aff6a9d384c9a06810aa8157116f6fef78
RAO Meenakshi <MRAO@deq.state.or.us>
2020-04-15 19:31:34 -0700

    Added loops to do multile pollutants.

--- daily_graphs_automation.R
+++ daily_graphs_automation.R
@@ -14,11 +14,13 @@ stn_info <- read.csv("data/stn_info.csv", stringsAsFactors = FALSE)
 stn_info$stn_id <- as.numeric(stn_info$stn_id)
 stn_info$latitude <- as.numeric(stn_info$latitude)
 neph_stns <- read.csv("data/stns_neph.csv", stringsAsFactors = FALSE)
-stations <- neph_stns[,2]
+no2_stns <- c(410510080, 410670005)
+o3_stns <- c()
+#stations <- neph_stns[,2]
 
 
 ## specify the years you want to use in the graphs
-yrs_to_get <- c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019)
+yrs_to_get <- c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018)
 
 ## speicfy the pollutants you want the graphs for
 pollutants_to_get <- c("88502")
@@ -46,53 +48,62 @@ pollutants_to_get <- c("88502")
 ##     first quarter: "q1"
 
 months_to_plot <- "to4"
-for (sta in stations) {
-  sta_name <- tolower(stn_info$code[stn_info$stn_id == sta])
-  df10 <- get_stn_data(stn = sta, yrs_to_get)
-  coln <- paste0(sta_name, "_pm25")
-  df19 <- get_website_data("data/or_neph_pm25_hourly_20190101_to_20200412_working.csv", sta, coln )
-  df <- rbind(df10, df19)
-  df <- arrange(df, dt_local)
-  df_daily <- create_daily_summary(df)
-  city_name <- stn_info$city[stn_info$stn_id == sta]
-  tstr <- paste0("Daily Average PM2.5 at ", toupper(sta_name),": ", city_name)
-  gr_pt <- make_point_graph(df_daily, months_to_plot, tstr)
-  gr_pt
-  gr_ln <- make_line_graph(df_daily, months_to_plot, 2020, tstr)
-  gr_ln
-  gr_tl <- make_tile_graph(df_daily, months_to_plot, tstr)
-  gr_tl
-  fname_dly <- paste0("summaries/", sta_name, "_", months_to_plot, "_daily_summary_201001-202004.csv")
-  write.csv(df_daily, fname_dly, row.names = FALSE)
-  line_dly <- paste0("graphs/", sta_name, "_daily_", months_to_plot, "_line_2020.png")
-  png(filename=line_dly, type="cairo", width = 900, height = 560)
-  plot(gr_ln)
-  dev.off()
-  gr_wline <- make_wline_graph(df_daily, months_to_plot, 2020, tstr)
-  wline_dly <- paste0("graphs/", sta_name, "_daily_", months_to_plot, "_wline_2020.png")
-  png(filename=wline_dly, type="cairo", width = 900, height = 560)
-  plot(gr_wline)
-  dev.off()
-  
-  pt_dly <- paste0("graphs/", sta_name,  "_daily_", months_to_plot,"_point_2020.png")
-  png(filename=pt_dly, type="cairo", width = 900, height = 560)
-  plot(gr_pt)
-  dev.off()
-  tile_dly <- paste0("graphs/", sta_name, "_daily_", months_to_plot, "_tile_2020.png")
-  png(filename=tile_dly, type="cairo", width = 900, height = 560)
-  plot(gr_tl)
-  dev.off()
+no2_stns <- c(410510080, 410670005)
+o3_stns <- c(410510080,  410670005)
+polls <- c("no2", "o3", "pm25")
+for (poll in polls) {
+  if (poll == "pm25") {
+    stations <- neph_stns[,2]
+    pollutant_to_get <- "88502"
+    webfile <- "data/or_neph_pm25_hourly_20190101_to_20200412_working.csv"
+  }
+  if (poll == "o3") {
+    stations <- o3_stns
+    pollutant_to_get <- "44201"
+    webfile <- "data/or_o3_hourly_20190101_to_20200412_working.csv"
+  }
+  if (poll == "no2") {
+    stations <- no2_stns
+    pollutant_to_get == "42602"
+    webfile <- "data/or_no2_hourly_20190101_to_20200412_working.csv"
+  }
+  for (sta in stations) {
+    sta_name <- tolower(stn_info$code[stn_info$stn_id == sta])
+    df10 <- get_stn_data(stn = sta, yrs_to_get, pollutant_to_get)
+    coln <- paste0(sta_name, "_", poll)
+    df19 <- get_website_data(webfile, sta, coln )
+    df <- rbind(df10, df19)
+    df <- arrange(df, dt_local)
+    df_daily <- create_daily_summary(df)
+    city_name <- stn_info$city[stn_info$stn_id == sta]
+    tstr <- paste0("Daily Average ",  toupper(poll), " at ", toupper(sta_name),": ", city_name)
+    gr_pt <- make_point_graph(df_daily, months_to_plot, tstr)
+    gr_pt
+    gr_ln <- make_line_graph(df_daily, months_to_plot, 2020, tstr)
+    gr_ln
+    gr_tl <- make_tile_graph(df_daily, months_to_plot, tstr)
+    gr_tl
+    fname_dly <- paste0("summaries/", sta_name, "_", poll, "_daily_summary_201001-202004.csv")
+    write.csv(df_daily, fname_dly, row.names = FALSE)
+    line_dly <- paste0("graphs/", sta_name, "_", poll, "_daily_", months_to_plot, "_line_2020.png")
+    png(filename=line_dly, type="cairo", width = 900, height = 560)
+    plot(gr_ln)
+    dev.off()
+    gr_wline <- make_wline_graph(df_daily, months_to_plot, 2020, tstr)
+    wline_dly <- paste0("graphs/", sta_name, "_", poll, "_daily_", months_to_plot, "_wline_2020.png")
+    png(filename=wline_dly, type="cairo", width = 900, height = 560)
+    plot(gr_wline)
+    dev.off()
+    
+    pt_dly <- paste0("graphs/", sta_name, "_", poll,  "_daily_", months_to_plot,"_point_2020.png")
+    png(filename=pt_dly, type="cairo", width = 900, height = 560)
+    plot(gr_pt)
+    dev.off()
+    tile_dly <- paste0("graphs/", sta_name, "_", poll, "_daily_", months_to_plot, "_tile_2020.png")
+    png(filename=tile_dly, type="cairo", width = 900, height = 560)
+    plot(gr_tl)
+    dev.off()
+  }
 }
 
-for (sta in stations) {
-  sta_name <- tolower(stn_info$code[stn_info$stn_id == sta])
-  tstr <- paste0("Daily Average PM2.5 at ", toupper(sta_name),": ", city_name)
-  fname <- paste0("summaries/", sta_name, "daily_", months_to_plot,"_daily_summary_201001-202004.csv" )
-  df_dly <- read.csv(fname, stringsAsFactors = FALSE)
-  gr_wline <- make_wline_graph(df_dly, months_to_plot, 2020, tstr)
-  wline_dly <- paste0("graphs/", sta_name, "_daily_", months_to_plot, "_wline_2020.png")
-  png(filename=wline_dly, type="cairo", width = 900, height = 560)
-  plot(gr_wline)
-  dev.off()
-  
-}
+
--- weekly_graphs_automation.R
+++ weekly_graphs_automation.R
@@ -3,7 +3,7 @@ library(lubridate)
 library(feather)   ## feather is a format for fast read & write of large files
 library(ggplot2)
 #library(hashmap)    
-library(zeallot)    ## %<-% operator for multiple assignments
+#library(zeallot)    ## %<-% operator for multiple assignments
 
 source("get_monitor_data.R")
 source("create_summaries.R")
@@ -20,30 +20,52 @@ stations <- neph_stns[,2]
 
 
 ## specify the years you want to use in the graphs
-yrs_to_get <- c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019)
+yrs_to_get <- c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018 )
 
-## speicfy the pollutants you want the graphs for
-pollutants_to_get <- c("88502")
+
 
 week_to_plot <- 14
-
-for (sta in stations) {
-  sta_name <- tolower(stn_info$code[stn_info$stn_id == sta])
-  df10 <- get_stn_data(stn = sta, yrs_to_get)
-  coln <- paste0(sta_name, "_pm25")
-  df19 <- get_website_data("data/or_neph_pm25_hourly_20190101_to_20200412_working.csv", sta, coln )
-  df <- rbind(df10, df19)
-  df <- arrange(df, dt_local)
-  df_weekly <- create_weekly_summary(df)
-  fname_wkly <- paste0("summaries/", sta_name, "_pm25_weekly_summary_201001-202003.csv")
-  write.csv(df_weekly, fname_wkly, row.names = FALSE)
+months_to_plot <- "to4"
+no2_stns <- c(410510080, 410670005)
+o3_stns <- c(410510080,  410670005)
+polls <- c("no2", "o3")
+for (poll in polls) {
+  if (poll == "pm25") {
+    stations <- neph_stns[,2]
+    pollutant_to_get <- "88502"
+    webfile <- "data/or_neph_pm25_hourly_20190101_to_20200412_working.csv"
+  }
+  if (poll == "o3") {
+    stations <- o3_stns
+    pollutant_to_get <- "44201"
+    webfile <- "data/or_o3_hourly_20190101_to_20200412_working.csv"
+  }
+  if (poll == "no2") {
+    stations <- no2_stns
+    pollutant_to_get == "42602"
+    webfile <- "data/or_no2_hourly_20190101_to_20200412_working.csv"
+  }
+  for (sta in stations) {
+    sta_name <- tolower(stn_info$code[stn_info$stn_id == sta])
+    df10 <- get_stn_data(stn = sta, yrs_to_get)
+    coln <- paste0(sta_name, "_", poll)
+    df19 <- get_website_data(webfile, sta, coln )
+    df <- rbind(df10, df19)
+    df <- arrange(df, dt_local)
+    df_weekly <- create_weekly_summary(df)
+    fname_wkly <- paste0("summaries/", sta_name, "_", poll, "_weekly_summary_201001-202003.csv")
+    write.csv(df_weekly, fname_wkly, row.names = FALSE)
+    
+    city_name <- stn_info$city[stn_info$stn_id == sta]
+    tstr <- paste0("Average ", toupper(poll), " Week ", as.character(week_to_plot), " ", toupper(sta_name),": ", city_name)
+    gr_box <- create_boxplot(df_weekly, week_to_plot, 2020, title_string = tstr)
+    gr_box
+    box_wkly <- paste0("graphs/", sta_name, "_", poll, "_box_week", as.character(week_to_plot), "_2020.png")
+    png(filename=box_wkly, type="cairo", width = 400, height = 450)
+    plot(gr_box)
+    dev.off()
+  }
   
-  city_name <- stn_info$city[stn_info$stn_id == sta]
-  tstr <- paste0("Average PM2.5 Week ", week_to_plot, " ", toupper(sta_name),": ", city_name)
-  gr_box <- create_boxplot(df_weekly, week_to_plot, 2020, title_string = tstr)
-  gr_box
-  box_wkly <- paste0("graphs/", sta_name, "_box_week", as.character(week_to_plot), "_2020.png")
-  png(filename=box_wkly, type="cairo", width = 400, height = 450)
-  plot(gr_box)
-  dev.off()
 }
+
+
